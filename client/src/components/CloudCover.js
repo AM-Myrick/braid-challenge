ca190e9e1fdbfefd95ef2c884d9047e588fb7f9b
Andre Myrick <amyrick.820@gmail.com>
2019-04-17 17:17:09 -0400

    adds normalize.css and update app and cloudcover.css

--- client/src/components/CloudCover.js
+++ client/src/components/CloudCover.js
@@ -11,7 +11,7 @@ const CloudCover = props => {
                 if (cloudConditions.length === 0) {
                     return (
                         <div key={idx} className="cloud-cover-no-issues">
-                            <h1>{props.cityDict[weatherStation]}</h1>
+                            <h2>{props.cityDict[weatherStation]}</h2>
                             <h2>Clear Skies!</h2>
                         </div>
                     )
@@ -19,15 +19,15 @@ const CloudCover = props => {
                 else if (cloudConditions.length > 1) {
                     return (
                         <div key={idx} className="cloud-cover-issues">
-                            <h1>{props.cityDict[weatherStation]}</h1>
-                            <h2>{cloudConditions[0].type} at altitude {cloudConditions[0].altitude} and {cloudConditions[1].type} at altitude {cloudConditions[1].altitude}</h2>
+                            <h2>{props.cityDict[weatherStation]}</h2>
+                            <h2 className="warning">{cloudConditions[0].type} at altitude {cloudConditions[0].altitude}<br/>and<br/>{cloudConditions[1].type} at altitude {cloudConditions[1].altitude}</h2>
                         </div>
                     )
                 }
                 else {
                     return (
                         <div key={idx} className="cloud-cover-issues">
-                            <h1>{props.cityDict[weatherStation]}</h1>
+                            <h2>{props.cityDict[weatherStation]}</h2>
                             <h2>{cloudConditions.type} at altitude {cloudConditions.altitude}</h2>
                         </div>
                     )
@@ -35,7 +35,7 @@ const CloudCover = props => {
             })}
         </div>:
         <div>
-            <h1>Loading cloud cover data...</h1>
+            <h2>Loading cloud cover data...</h2>
         </div>
     );
 };
