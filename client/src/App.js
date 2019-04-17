75a4e9e3562519fcc5cdb5f6a062dc45f01e02e3
AM_Myrick <amyrick.820@gmail.com>
2019-04-17 12:49:41 -0400

    adds storing data from the back-end as state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentDidMount() {
+    axios
+    .get("http://localhost:9001/weather-api")
+    .then(res => {
+      this.setState({weather: res.data})
+    })
+    .catch(err => console.log(err, "could not connect to back-end"))
+  }
+  
   render() {
     return (
       <div className="App">
